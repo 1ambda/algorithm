e2e8db4f15d5da39641188b09f39a1403dca497a
ansterd <ansterd@nate.com>
2014-03-30 22:01:44 +0900

    Impl : merge, insertion, bubble, selection sort

---
+++ cpp/sort-collection/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <functional>
+#include "sort.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int arr[10] = { 1, 1, 3, 8, 1, 4, 2, 7, 10, 15 };
+
+    function<bool(int&, int&)> f = [](int& l, int&r)->bool {
+        if (l > r) {
+            return true;
+        }
+
+        return false;
+    };
+
+    // Sort<int>::bubble(arr, 10, f);
+    // Sort<int>::selection(arr, 10, f);
+    // Sort<int>::insertion(arr, 10, f);
+    Sort<int>::merge(arr, 10, f);
+    
+
+    for(int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " " << std::endl;
+    }
+    
+    return 0;
+}
---
+++ cpp/sort-collection/src/sort.h
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+template <typename T>
+class Sort
+{
+public:
+    Sort<T>();
+    virtual ~Sort<T>();
+
+    static bool bubble(T* arr, size_t length, function<bool(T& t1, T& t2)>& comparator);
+    static bool selection(T* arr, size_t length, function<bool(T& l, T& r)>& comparator);
+    static bool insertion(T* arr, size_t length, function<bool(T& l, T& r)>& comparator);
+    static bool merge(T* arr, int length, function<bool(T&, T&)>& comparator);
+
+private:
+    static void internal_merge(T* arr, int length, function<bool(T&, T&)>& comparator);
+};
+
+template <typename T>
+Sort<T>::Sort() {
+    
+}
+
+template <typename T>
+Sort<T>::~Sort() {
+    
+}
+
+template <typename T>
+bool Sort<T>::bubble(T* arr, size_t length, function<bool(T&, T&)>& comparator) {
+
+    if ( arr == nullptr ) {
+        return false;
+    }
+
+    if (length <= 1) {
+        return false;
+    }
+
+    bool flag = false;
+    
+    for(int i = length; i >1; i--) {
+        flag = false;
+        
+        for(int j = 0; j < i - 1; j++) {
+            if (comparator(arr[j], arr[j+1])) {
+                T* temp = new T(arr[j]);
+                arr[j] = arr[j+1];
+                arr[j+1] = *temp;
+                flag = true;
+            }
+        }
+
+        if (flag) {
+            break;
+        }
+    }
+
+    return true;
+}
+
+template <typename T>
+bool Sort<T>::selection(T* arr, size_t length, function<bool(T&, T&)>& comparator) {
+
+    if(arr == nullptr) {
+        return false;
+    }
+
+    if (length <= 1) {
+        return false;
+    }
+
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = i+1; j < length; j++) {
+            if( comparator(arr[i], arr[j]) ) {
+                T* temp = new T(arr[j]);
+                arr[j] = arr[i];
+                arr[i] = *temp;
+            }
+        }
+    }
+
+    return true;
+}
+
+template <typename T>
+bool Sort<T>::insertion(T* arr, size_t length, function<bool(T&, T&)>& comparator) {
+
+    if(arr == nullptr) {
+        return false;
+    }
+
+    if(length <= 1) {
+        return false;
+    }
+
+    int j = 0;
+
+    for(int i = 1; i < length; i++) {
+        T* temp = new T(arr[i]);
+
+        for(j = i - 1; j >= 0 && comparator(arr[j], *temp); j--)
+            arr[j+1] = arr[j];
+
+        arr[j+1] = *temp;
+    }
+
+    return true;
+}
+
+template <typename T>
+bool Sort<T>::merge(T* arr, int length, function<bool(T&, T&)>& comparator) {
+
+    if(arr == nullptr) {
+        return false;;
+    }
+
+    if (length <= 1) {
+        return false;
+    }
+
+    Sort<T>::internal_merge(arr, length, comparator);
+    
+    return true;
+}
+
+template <typename T>
+void Sort<T>::internal_merge(T* arr, int length, function<bool(T&, T&)>& f) {
+
+    if ( length > 1) {
+
+        int mid = length / 2;
+
+        internal_merge(arr, mid, f);
+        internal_merge(arr + mid, length - mid, f);
+
+        // merge two partial arrays
+        T* temp = new T[length];
+
+        int left, right, index;
+        for(index = 0, left = 0, right = mid; left < mid && right < length; index++) {
+            temp[index] = (f(arr[left], arr[right])) ? arr[right++] : arr[left++];
+        }
+
+        while(left < mid) temp[index++] = arr[left++];
+        while(right < length) temp[index++] = arr[right++];
+
+        for(index = 0; index < length; index++) {
+            arr[index] = temp[index];
+        }
+
+        delete temp;
+    }
+}
+
