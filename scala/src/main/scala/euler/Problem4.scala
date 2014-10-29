729fc608773d3573df1b1903b3173054b0f27ded
1ambda <1amb4a@gmail.com>
2014-10-29 12:36:53 +0900

    euler problem4

---
+++ scala/src/main/scala/euler/Problem4.scala
@@ -0,0 +1,21 @@
+package euler.Problem4
+
+object Problem4 extends App{
+
+  def isPalindrome(s: String): Boolean = (s.length >= 2) && s == s.reverse
+
+  def getMaxPdnum(n: Int): Int = {
+    def recur(a: Int, b: Int): Int = b match {
+      case 100 => 10001
+      case _ => {
+        val num = a * b
+        if (isPalindrome(num.toString())) num else recur(a, b-1)
+      }
+    }
+
+    recur(n, n)
+  }
+
+  val max = (999 to 100 by -1 map getMaxPdnum).max
+  println(max)
+}
---
+++ scala/src/test/scala/euler/Problem4Test.scala
@@ -0,0 +1,16 @@
+package euler.Problem4
+
+import org.scalatest._
+
+class Problem4Test extends FlatSpec with Matchers {
+
+  import Problem4._
+
+  "isPalindromeTest" should "pass" in {
+    assert(isPalindrome("") == false)
+    assert(isPalindrome("a") == false)
+    assert(isPalindrome("asdds") == false)
+    assert(isPalindrome("asddsa") == true)
+    assert(isPalindrome("asdsa") == true)
+  }
+}
