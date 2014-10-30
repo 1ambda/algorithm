86e5c10deff76a8c769b8ec4c34cb57dea5b65e2
1ambda <1amb4a@gmail.com>
2014-10-30 12:34:59 +0900

    euler prb 7

---
+++ scala/src/main/scala/euler/Problem7.scala
@@ -0,0 +1,34 @@
+package euler
+
+import scala.annotation.tailrec
+
+object Problem7 extends App {
+
+  def isPrime(n: Int): Boolean = {
+
+    val limit = n / 2
+
+    @tailrec
+    def recur(acc: Int): Boolean = {
+      if (acc > limit) true
+      else if (n % acc == 0) false
+      else recur(acc + 1)
+    }
+
+    recur(2)
+  }
+
+  def nthPrime(n: Int): Int = {
+
+    @tailrec
+    def npRecur(num: Int, count: Int): Int = isPrime(num) match {
+      case true => if (count == n) num else npRecur(num + 1, count + 1)
+      case false => npRecur(num + 1, count)
+    }
+
+    npRecur(2, 1)
+  }
+
+  println(nthPrime(10001))
+}
+
